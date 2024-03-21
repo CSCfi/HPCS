6483fb92d9d536cd902e81a893ab739dd7b05de2
telliere <emilien.tellier@hpe.com>
2024-03-21 18:55:09 +0200

    adapting job preparation to config file usage

--- utils/conf/client/conf.py
+++ utils/conf/client/conf.py
@@ -5,6 +5,9 @@ def parse_configuration(path : str):
     config = ConfigParser()
     config.read(path)
     
+    if not 'supercomputer' in config:
+        raise NoSectionError("supercomputer section missing in configuration file, aborting")
+    
     if not 'spire-server' in config:
         raise NoSectionError("hpcs-server section missing in configuration file, aborting")
     
@@ -13,6 +16,9 @@ def parse_configuration(path : str):
     
     if not 'vault' in config:
         raise NoSectionError("vault section missing in configuration file, aborting")
+    
+    if not 'address' in config['supercomputer'] or not 'username' in config['supercomputer']:
+        raise NoOptionError("'spire-server' section is incomplete in configuration file, aborting")
     
     if not 'address' in config['spire-server'] or not 'port' in config['spire-server'] or not 'trust-domain' in config['spire-server']:
         raise NoOptionError("'spire-server' section is incomplete in configuration file, aborting")
