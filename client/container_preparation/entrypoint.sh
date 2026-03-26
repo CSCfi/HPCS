57632b82ece8a88d54c03ef50c0b4f94e4a80fd8
Sam Hardwick <sam.hardwick@csc.fi>
2026-03-26 16:28:50 +0200

    Unify waiting for Spire agent socket
    First wait, then check if process died, then print that we're waiting
    some more. This way there's usually no concerning needless printing.

--- client/container_preparation/entrypoint.sh
+++ client/container_preparation/entrypoint.sh
@@ -131,12 +131,14 @@ if [ -n "$encrypted" ]; then
 	spire_agent_pid=$!
 
 	# Wait for socket to be created
+	sleep 2
 	until [ -e /tmp/agent.sock ]; do
-		sleep 2
-		if ! ps "$spire_agent_pid" >/dev/null 2>&1; then
-			echo "spire agent died, aborting"
-			end_entrypoint "$spire_agent_pid" 1
-		fi
+	    if ! ps "$spire_agent_pid" >/dev/null 2>&1; then
+		echo "spire agent died, aborting"
+		end_entrypoint "$spire_agent_pid" 1
+	    fi
+	    printf "%b\n" "${RED}[LUMI-SD][Data preparation] Spire workload api socket doesn't exist, waiting 5 seconds ${NC}"
+	    sleep 5
 	done
 
 	# Make socket accessible to all processes
